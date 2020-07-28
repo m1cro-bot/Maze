cfba49942f7ad699b1d2a2d0381ee804df42d420
Romy Setiawan <setiawan.romy@yahoo.co.id>
2020-07-28 19:42:11 +0700

    Fix

--- MazeGenerator.js
+++ MazeGenerator.js
@@ -1,86 +1,51 @@
-let S
-let n
-let posisi = [0, 1]
-let m = 0
-
-const takeInput = async (chunk) => { 
-    let input = await parseInt(chunk)
-    if (!input){
-        throw Error(`Input Harus Angka`)
-    }
-    S = input
-    n = S*S-1
-    return generateMaze()
-}
-
-const generateMaze = () =>{
-
-    const map = []
-    for (let i=0; i<S; i++){
-        map[i]=[]
-        for (let j=0; j<S; j++){
-            map[i].push(i>0 && i<S-1 && j>0 && j<S-1)
-        }
-    }
-    while(n>0){
-        let move = [[posisi[0]+1, posisi[1]], [posisi[0], posisi[1]+1], [posisi[0]-1, posisi[1]], [posisi[0], posisi[1]-1]]
-            if (m<4 && map[move[m][0]][move[m][1]]){
-                let next = [move[m][0], move[m][1]]
-                map[posisi[0]][posisi[1]] = 1
-                let moving = m
-                switch (moving) {
-                    case 0:
-                    case 2:
-                        if(map[posisi[0]][posisi[1]+1] !== 1){
-                            map[posisi[0]][posisi[1]+1] = false
-                        }
-                        if(map[posisi[0]][posisi[1]-1] !== 1){
-                            map[posisi[0]][posisi[1]-1] = false
-                        }
-                        break;
-                    case 1:
-                    case 3:
-                        if(map[posisi[0]+1][posisi[1]] !== 1){
-                            map[posisi[0]+1][posisi[1]] = false
-                        }
-                        if(map[posisi[0]-1][posisi[1]] !== 1){
-                            map[posisi[0]-1][posisi[1]] = false
-                        }
-                        break;
-                }
-                posisi = next
-            } else if(m>3){
-                m = 0
-            } else {
-                m = m+1
-            }
-        n--
-    }
-    drawMaze(map)
-}
-
-const drawMaze = (map) => {
-    let row = []
-    for(let i=0; i<S; i++){
-        let coll = []
-        for(let j=0; j<S; j++){
-            if(map[i][j]){
-                coll[j] = ' '
-            }
-            if(map[i][j]){
-                coll[j] = ' '
-            } else {
-                coll[j] = '@'
-            }
-        }
-        if(i === 0) coll[1] = ' '
-        row.push(coll.join('')+'\r\n')
-    }
-    let maze = row.join('')
-    return console.log(maze);
-
-}
-
-console.log('Maze Generator')
 console.log('Massukkan Ukuran Maze')
-process.stdin.once('data', takeInput)
+process.stdin.once('data', (takeInput) => {
+    let S = parseInt(takeInput)
+    let n = S*S-1
+    let posisi = [0, 1]
+    let m = 0
+    let draw = []
+    let map = []
+    for(let i=0; i<S; i++){
+        map[i] = []
+        draw[i] = []
+        for(let j=0; j<S; j++){
+            draw[i].push('@')
+            map[i].push(i>0 && i<S-1 && j>0 && j<S-1 && (i != posisi[0] || j != posisi[1]))
+        }
+    }
+    while( n>0 ){
+        let move = [[posisi[0]+1, posisi[1]], [posisi[0], posisi[1]+1], [posisi[0]-1, posisi[1]], [posisi[0], posisi[1]-1]]
+        if (m<4 && map[move[m][0]][move[m][1]]){
+            let next = [move[m][0], move[m][1]]
+            draw[posisi[0]][posisi[1]] = ' '
+            switch (m) {
+                case 0:
+                case 2:
+                    map[posisi[0]][posisi[1]+1] = false
+                    map[posisi[0]][posisi[1]-1] = false
+                    break;
+                case 1:
+                case 3:
+                    map[posisi[0]+1][posisi[1]] = false
+                    map[posisi[0]-1][posisi[1]] = false
+                    break;
+            }
+            posisi = next
+        } else if(m>3){
+            m = 0
+        } else {
+            m = m+1
+        }
+        n = n-1
+    }
+    let text = []
+    for(let i=0; i<S; i++){
+        let line = []
+        for(let j=0; j<S; j++){
+            line[j] = draw[i][j]
+        }
+        text.push(line.join('')+'\r\n')
+    }
+    return console.log(text.join(''));
+})
